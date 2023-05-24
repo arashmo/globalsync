64f2eeae8f87f94f66b24e1daefb74a2fc997210
Mohammadreza Mohammadi <mohammadrezamohammadi@Mohammadrezas-MacBook-Air.local>
2023-05-24 14:25:24 -0400

    bettwer way to handle data update

--- db/database.go
+++ db/database.go
@@ -3,7 +3,6 @@ package db
 import (
 	"database/sql"
 	"log"
-
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -13,15 +12,144 @@ func Connect(dsn string) {
 	var err error
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error opening database: %v", err)
 	}
+
 	if err := DB.Ping(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error connecting to database: %v", err)
 	}
+
+	// Call the InitTables function here to ensure tables are created if they do not exist
+	InitTables()
 }
 
 func Close() {
 	if DB != nil {
-		DB.Close()
+		if err := DB.Close(); err != nil {
+			log.Fatalf("Error closing database: %v", err)
+		}
 	}
+}
+
+func InitTables() {
+    // Define table creation queries
+    createDatacentersQuery := `
+        CREATE TABLE IF NOT EXISTS datacenters (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255),
+            location VARCHAR(255),
+            comment VARCHAR(255)
+        )
+    `
+    createRacksQuery := `
+        CREATE TABLE IF NOT EXISTS racks (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            number INT,
+            aisle_number INT,
+            location VARCHAR(255),
+            datacenter_id INT,
+            FOREIGN KEY (datacenter_id) REFERENCES datacenters(id)
+        )
+    `
+    createOwnerGroupsQuery := `
+        CREATE TABLE IF NOT EXISTS owner_groups (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255)
+        )
+    `
+    createDatasetsQuery := `
+        CREATE TABLE IF NOT EXISTS datasets (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255),
+            size INT,
+            version VARCHAR(255),
+            status VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )
+    `
+    createServersQuery := `
+        CREATE TABLE IF NOT EXISTS servers (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            hostname VARCHAR(255),
+            ip_address VARCHAR(255),
+            cpupercent INT,
+            usedgpus INT,
+            freedisk INT,
+            rack_id INT,
+            datacenter_id INT,
+            FOREIGN KEY (rack_id) REFERENCES racks(id),
+            FOREIGN KEY (datacenter_id) REFERENCES datacenters(id)
+        )
+    `
+    createServerOwnerGroupsQuery := `
+        CREATE TABLE IF NOT EXISTS server_owner_groups (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            server_id INT,
+            owner_group_id INT,
+            FOREIGN KEY (server_id) REFERENCES servers(id),
+            FOREIGN KEY (owner_group_id) REFERENCES owner_groups(id)
+        )
+    `
+    createServerDatasetsQuery := `
+        CREATE TABLE IF NOT EXISTS server_datasets (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            server_id INT,
+            dataset_id INT,
+            attached_storage_id INT,
+            folder_name VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (server_id) REFERENCES servers(id),
+            FOREIGN KEY (dataset_id) REFERENCES datasets(id)
+        )
+    `
+    createAttachedStorageQuery := `
+        CREATE TABLE IF NOT EXISTS attached_storage (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            server_id INTEGER,
+            location VARCHAR(255) NOT NULL,
+            UNIQUE (server_id, location),
+            FOREIGN KEY (server_id) REFERENCES servers(id)
+        )
+    `
+
+    // Execute each query
+    _, err := DB.Exec(createDatacentersQuery)
+    if err != nil {
+        log.Fatalf("Error creating datacenters table: %v", err)
+    }
+    
+    _, err = DB.Exec(createRacksQuery)
+    if err != nil {
+        log.Fatalf("Error creating racks table: %v", err)
+    }
+    
+    _, err = DB.Exec(createOwnerGroupsQuery)
+    if err != nil {
+        log.Fatalf("Error creating owner_groups table: %v", err)
+    }
+    
+    _, err = DB.Exec(createDatasetsQuery)
+    if err != nil {
+        log.Fatalf("Error creating datasets table: %v", err)
+    }
+    
+    _, err = DB.Exec(createServersQuery)
+    if err != nil {
+        log.Fatalf("Error creating servers table: %v", err)
+    } 
+	_, err = DB.Exec(createServerDatasetsQuery)
+    if err != nil {
+        log.Fatalf("Error creating servers table: %v", err)
+    }
+    
+    _, err = DB.Exec(createServerOwnerGroupsQuery)
+    if err != nil {
+        log.Fatalf("Error creating server_owner_groups table: %v", err)
+    }  
+	 _, err = DB.Exec(createAttachedStorageQuery)
+    if err != nil {
+        log.Fatalf("Error creating server_owner_groups table: %v", err)
+    }
+    
 }
--- db/mysql.sql
+++ db/mysql.sql
@@ -1,3 +1,12 @@
+
+DROP TABLE IF EXISTS server_datasets;
+DROP TABLE IF EXISTS attached_storage;
+DROP TABLE IF EXISTS servers;
+DROP TABLE IF EXISTS datasets;
+DROP TABLE IF EXISTS owner_groups;
+DROP TABLE IF EXISTS racks;
+DROP TABLE IF EXISTS datacenters;
+
 CREATE TABLE datacenters (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255),
@@ -24,7 +33,9 @@ CREATE TABLE datasets (
   name VARCHAR(255),
   size INT,
   version VARCHAR(255),
-  status VARCHAR(255)
+  status VARCHAR(255),
+  creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE servers (
@@ -33,80 +44,37 @@ CREATE TABLE servers (
   ip_address VARCHAR(255),
   cpupercent INT,
   usedgpus INT,
-  owner_group_id INT,
   freedisk INT,
   rack_id INT,
   datacenter_id INT,
-  FOREIGN KEY (datacenter_id) REFERENCES datacenters(id),
   FOREIGN KEY (rack_id) REFERENCES racks(id),
+  FOREIGN KEY (datacenter_id) REFERENCES datacenters(id)
+);
+
+CREATE TABLE server_owner_groups (
+  server_id INT,
+  owner_group_id INT,
+  PRIMARY KEY (server_id, owner_group_id),
+  FOREIGN KEY (server_id) REFERENCES servers(id),
   FOREIGN KEY (owner_group_id) REFERENCES owner_groups(id)
 );
 
-
-
+CREATE TABLE attached_storage (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  server_id INT,
+  location VARCHAR(255) NOT NULL,
+  FOREIGN KEY (server_id) REFERENCES servers(id),
+  UNIQUE KEY (server_id, location)
+);
 
 CREATE TABLE server_datasets (
-    id SERIAL PRIMARY KEY,
-    server_id INT NOT NULL REFERENCES servers(id),
-    dataset_id INT NOT NULL REFERENCES datasets(id),
-    attached_storage_id INT NOT NULL REFERENCES attached_storage(id),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    server_id INT NOT NULL,
+    dataset_id INT NOT NULL,
+    attached_storage_id INT NOT NULL,
     folder_name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (server_id) REFERENCES servers(id),
+    FOREIGN KEY (dataset_id) REFERENCES datasets(id),
+    FOREIGN KEY (attached_storage_id) REFERENCES attached_storage(id)
 );
-
-CREATE TABLE attached_storage (
-  id SERIAL PRIMARY KEY,
-  server_id INTEGER REFERENCES servers(id),
-  location VARCHAR(255) NOT NULL,
-  UNIQUE (server_id, location)
-);
-
-
-
---datacenters table
-INSERT INTO datacenters (name, location, comment) VALUES
-('Datacenter 1', 'New York', 'Primary datacenter'),
-('Datacenter 2', 'Los Angeles', 'Secondary datacenter');
-
--- racks table
-INSERT INTO racks (number, aisle_number, location, datacenter_id) VALUES
-(1, 1, 'North-East corner', (SELECT id FROM datacenters ORDER BY RAND() LIMIT 1)),
-(2, 1, 'North-West corner', (SELECT id FROM datacenters ORDER BY RAND() LIMIT 1)),
-(3, 1, 'South-East corner', (SELECT id FROM datacenters ORDER BY RAND() LIMIT 1)),
-(4, 1, 'South-West corner', (SELECT id FROM datacenters ORDER BY RAND() LIMIT 1));
-
--- owner_groups table
-INSERT INTO owner_groups (name) VALUES
-('Group A'),
-('Group B'),
-('Group C');
-
--- datasets table
-INSERT INTO datasets (name, version) VALUES
-('Dataset 1', 'v1.0'),
-('Dataset 2', 'v2.0'),
-('Dataset 3', 'v1.5');
-
--- servers table
-INSERT INTO servers (hostname, ip_address, cpupercent, usedgpus, freedisk, owner_group_id, rack_id, datacenter_id) VALUES
-('server1', '192.168.1.1', 20, 2, 500, (SELECT id FROM owner_groups ORDER BY RAND() LIMIT 1), (SELECT id FROM racks ORDER BY RAND() LIMIT 1), (SELECT id FROM datacenters ORDER BY RAND() LIMIT 1)),
-('server2', '192.168.1.2', 10, 0, 1000, (SELECT id FROM owner_groups ORDER BY RAND() LIMIT 1), (SELECT id FROM racks ORDER BY RAND() LIMIT 1), (SELECT id FROM datacenters ORDER BY RAND() LIMIT 1)),
-('server3', '192.168.2.1', 30, 4, 200, (SELECT id FROM owner_groups ORDER BY RAND() LIMIT 1), (SELECT id FROM racks ORDER BY RAND() LIMIT 1), (SELECT id FROM datacenters ORDER BY RAND() LIMIT 1)),
-('server4', '192.168.2.2', 5, 1, 750, (SELECT id FROM owner_groups ORDER BY RAND() LIMIT 1), (SELECT id FROM racks ORDER BY RAND() LIMIT 1), (SELECT id FROM datacenters ORDER BY RAND() LIMIT 1));
-
--- server_datasets table
-INSERT INTO server_datasets (dataset_id, server_id, attached_storage_id, folder_name) VALUES
-    ((SELECT id FROM datasets ORDER BY RAND() LIMIT 1), (SELECT id FROM servers ORDER BY RAND() LIMIT 1), (SELECT id FROM attached_storage ORDER BY RAND() LIMIT 1),'data_folder_1'),
-    ((SELECT id FROM datasets ORDER BY RAND() LIMIT 1), (SELECT id FROM servers ORDER BY RAND() LIMIT 1), (SELECT id FROM attached_storage ORDER BY RAND() LIMIT 1),'data_folder_2'),
-    ((SELECT id FROM datasets ORDER BY RAND() LIMIT 1), (SELECT id FROM servers ORDER BY RAND() LIMIT 1), (SELECT id FROM attached_storage ORDER BY RAND() LIMIT 1),'data_folder_3');
- 
-
-
--- attached_storage data_entry 
-INSERT INTO attached_storage (server_id, location) VALUES
-  ((SELECT id FROM servers ORDER BY RAND() LIMIT 1), '/path/to/attached/storage/for/server1'),
-  ((SELECT id FROM servers ORDER BY RAND() LIMIT 1), '/path/to/attached/storage/for/server2'),
-  ((SELECT id FROM servers ORDER BY RAND() LIMIT 1), '/path/to/attached/storage/for/server3');
-
-----
-
